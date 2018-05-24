f0b787fc237f4bd40b2e6aaac7712c9f142e9d27
Mervyn Wong <mervynrwtw@gmail.com>
2018-05-24 20:26:42 +0800

    2

--- src/App.js
+++ src/App.js
@@ -13,45 +13,74 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import { withTheme } from '@material-ui/core/styles'
 
-const codes = VoucherCodes.generate({
-  charset: "BCDFGHJKMPQRTVWXY234789",
-  length: 8,
-  count: 5
-});
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-const listItems = codes.map((code) =>
-  <TableRow key={code}>
-    <TableCell component="th" scope="row">
-      {code}
-    </TableCell>
-  </TableRow>
-);
+    this.state = {
+      length: 8,
+      count: 5,
+    }
+  }
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
 
-function App(props) {
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Promo Code Generator
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Promo Codes</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {listItems}
-          </TableBody>
-        </Table>
-      </Paper>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Promo Code Generator
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <TextField
+            id="length"
+            label="length"
+            value={this.state.length}
+            onChange={this.handleChange('length')}
+            margin="normal"
+          />
+        <br />
+          <TextField
+            id="count"
+            label="count"
+            value={this.state.count}
+            onChange={this.handleChange('count')}
+            margin="normal"
+          />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Promo Codes</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                VoucherCodes.generate({
+                  charset: "BCDFGHJKMPQRTVWXY234789",
+                  length: this.state.length,
+                  count: this.state.count
+                }).map((code) =>
+                  <TableRow key={code}>
+                    <TableCell component="th" scope="row">
+                      {code}
+                    </TableCell>
+                  </TableRow>
+                )
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    )
+  }
 }
 
 
